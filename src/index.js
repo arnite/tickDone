0fea896b23ad09ceca43730d6c52ba2d68ad6c40
arnite <udembao429@gmail.com>
2025-01-20 20:39:48 +0100

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -6,47 +6,69 @@ const integrateDB = require('../src/config/db');
 const superAdmin = require('./config/superAdmin');
 
 const startApp = async () => {
-  await integrateDB();
-  await superAdmin();
-  console.log('tickDone is ready');
-};
-
-const startCLI = async () => {
-  const { action } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'action',
-      message: 'What would you like to do?',
-      choices: ['Register', 'Login', 'Exit'],
-    },
-  ]);
-
-  if (action === 'Register') {
-    const { name, email, password, passwordConfirm } = await inquirer.prompt([
-      { type: 'input', name: 'name', message: 'Enter your name:' },
-      { type: 'input', name: 'email', message: 'Enter your email:' },
-      { type: 'password', name: 'password', message: 'Enter your password:' },
-      {
-        type: 'password',
-        name: 'passwordConfirm',
-        message: 'confirm your password:',
-      },
-    ]);
-    await registerUser(name, email, password, passwordConfirm);
-  } else if (action === 'Login') {
-    const { email, password } = await inquirer.prompt([
-      { type: 'input', name: 'email', message: 'Enter your email:' },
-      { type: 'password', name: 'password', message: 'Enter your password:' },
-    ]);
-    await loginUser(email, password);
-  } else {
-    console.log('Goodbye!');
-    process.exit(0);
+  try {
+    await integrateDB();
+    await superAdmin();
+    console.log('tickDone is ready');
+  } catch (error) {
+    console.error('Error starting the app:', error.message);
+    process.exit(1); // Exit with failure code
   }
 };
 
-//Start the App
-startApp();
+const startCLI = async () => {
+  try {
+    while (true) {
+      const { action } = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: ['Register', 'Login', 'Exit'],
+        },
+      ]);
 
-//Initiate the CLI
-startCLI();
+      if (action === 'Register') {
+        const { name, email, password, passwordConfirm } =
+          await inquirer.prompt([
+            { type: 'input', name: 'name', message: 'Enter your name:' },
+            { type: 'input', name: 'email', message: 'Enter your email:' },
+            {
+              type: 'password',
+              name: 'password',
+              message: 'Enter your password:',
+            },
+            {
+              type: 'password',
+              name: 'passwordConfirm',
+              message: 'Confirm your password:',
+            },
+          ]);
+        await registerUser(name, email, password, passwordConfirm);
+      } else if (action === 'Login') {
+        const { email, password } = await inquirer.prompt([
+          { type: 'input', name: 'email', message: 'Enter your email:' },
+          {
+            type: 'password',
+            name: 'password',
+            message: 'Enter your password:',
+          },
+        ]);
+        await loginUser(email, password);
+      } else if (action === 'Exit') {
+        console.log('Goodbye!');
+        process.exit(0);
+      }
+    }
+  } catch (error) {
+    console.error('An error occurred in the CLI:', error.message);
+  }
+};
+
+// Main function to start the app and CLI
+const main = async () => {
+  await startApp();
+  await startCLI();
+};
+
+main();
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -106,4 +106,4 @@ userSchema.methods.createPasswordResetToken = function () {
 };
 
 const User = mongoose.model('User', userSchema);
-module.exports = { User };
+module.exports = User;
